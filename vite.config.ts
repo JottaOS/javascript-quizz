e208a019f4b9862dfd3f1e6a2e65b2f713b2dbc3
JuanMendozaOS <juanmendozasfa@gmail.com>
2024-05-19 16:44:33 -0400

    fix: gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: 'https://juanmendozaos.github.io/javascript-quizz/' // Link page
 })
